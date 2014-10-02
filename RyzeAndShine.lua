246f14fb54dd4fa601e98efe6b470f9c5789af1d
Garag14 <garag@gmx.at>
2014-10-02 21:26:02 +0200

    Release Update

--- RyzeAndShine.lua
+++ RyzeAndShine.lua
@@ -1,4 +1,4 @@
-local version = "1.000"
+local version = "1.001"
 
 --[[
 
@@ -33,6 +33,7 @@ local version = "1.000"
 						Jungle-Clear added as own Submenu, but not working yet, dont know if you realy need it...
 			1.000		Completely Rewrote the Script, cause since Patch 4.11 it was broken
 						First Remake Release
+			1.001 General Bugfixing after first Release of Remake
 						
 ]]--
 
